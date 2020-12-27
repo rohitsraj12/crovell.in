954c409f554ea8c478ae9179a964c89907a512d2
rohitsraj12 <rohitsraj12@gmail.com>
2020-12-27 09:52:25 +0530

    Create other.php

---
+++ public/other.php
@@ -0,0 +1,120 @@
+<?php include("header.php") ?>
+	
+ 
+
+
+	<!-- fEatured pRoduct Area -->
+		<div class="inner-page">
+			<div class="container-fluid">
+				<div class="row">	
+					<div class="inner-banner">
+						<img src="images/inner-banner.jpg" class="img-responsive"/>
+					</div>
+					<div class="inner-page-content">
+					
+							<div class="col-md-12">
+								<!-- cRovell pRoduct aRea-->
+		<div class="accessories-section">
+			<div class="container">
+				<div class="row">
+				<div class="col-md-12">	
+					<h4><span>Mobile Other Accessories</span></h4>
+				</div>
+				<div class="col-md-12">
+					<div class="col-sm-3 col-md-3">
+						<div class="accessories-body">
+							<div class="accessories-panel wow fadeInLeft" data-wow-delay="0.2s" >
+								<img src="images/other/other8.png" class="img-responsive" alt="" />
+							</div>
+							<div class="accessories-footer">
+								<h3>mobile Stand</h3>
+							</div>
+							
+						</div>
+					</div>
+				
+					<div class="col-sm-3 col-md-3">
+						<div class="accessories-body">
+							<div class="accessories-panel wow fadeInLeft" data-wow-delay="0.4s">
+								<img src="images/other/other13.png" class="img-responsive" alt="" />
+							</div>
+							<div class="accessories-footer">
+								<h3>mobile Wall Stand</h3>
+							</div>
+						</div>
+					</div>
+			
+			
+					<div class="col-sm-3 col-md-3">
+						<div class="accessories-body">
+							<div class="accessories-panel wow fadeInLeft" data-wow-delay="0.2s" >
+								<img src="images/other/other1.png" class="img-responsive" alt="" />
+							</div>
+							<div class="accessories-footer">
+								<h3>Mobile Battery OG Series</h3>
+							</div>
+						</div>
+					</div>
+					
+					<div class="col-sm-3 col-md-3">
+						<div class="accessories-body">
+							<div class="accessories-panel wow fadeInLeft" data-wow-delay="0.6s">
+								<img src="images/other/other3.png" class="img-responsive" alt="" />
+							</div>
+							<div class="accessories-footer">
+								<h3>Mobile Battery OG Series</h3>
+							</div>
+						</div>
+					</div>	
+		
+
+
+						<div class="col-sm-3 col-md-3">
+							<div class="accessories-body">
+								<div class="accessories-panel wow fadeInLeft" data-wow-delay="0.2s" >
+									<img src="images/other/other6.png" class="img-responsive" alt="" />
+								</div>
+								<div class="accessories-footer">
+									<h3>Mobile Glass</h3>
+								</div>
+							</div>
+						</div>
+					<div class="col-sm-3 col-md-3">
+						<div class="accessories-body">
+							<div class="accessories-panel wow fadeInLeft" data-wow-delay="0.4s">
+								<img src="images/other/other7.png" class="img-responsive" alt="" />
+							</div>
+							<div class="accessories-footer">
+									<h3>Mobile Glass</h3>
+								</div>
+						</div>
+					</div>
+					
+				
+					
+					<div class="col-sm-3 col-md-3">
+						<div class="accessories-body">
+							<div class="accessories-panel wow fadeInLeft" data-wow-delay="0.2s" >
+								<img src="images/other/other5.png" class="img-responsive" alt="" />
+							</div>
+							<div class="accessories-footer">
+									<h3>Mobile Body</h3>
+								</div>
+						</div>
+					</div>					
+					
+
+				</div>				
+			</div>
+		</div>
+							</div>
+							
+						</div>
+								
+				</div>				
+			</div>
+		</div>
+	<!--eNd fEatured pRoduct Area -->
+
+	
+	<?php include("footer.php"); ?>
