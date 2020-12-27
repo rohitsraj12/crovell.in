f1e2b4c592f70779efca24c79465c99238b6aabb
rohitsraj12 <rohitsraj12@gmail.com>
2020-12-27 09:47:32 +0530

    Create headset.php

---
+++ public/headset.php
@@ -0,0 +1,154 @@
+<?php include("header.php") ?>
+	
+ 
+
+
+	<!-- fEatured pRoduct Area -->
+		<div class="inner-page">
+			<div class="container-fluid">
+				<div class="row">	
+					<div class="inner-banner">
+						<img src="images/inner-banner.jpg" class="img-responsive"/>
+					</div>
+					<div class="inner-page-content">
+					
+							<div class="col-md-12">
+								<!-- cRovell pRoduct aRea-->
+		<div class="accessories-section">
+			<div class="container">
+				<div class="row">
+				<div class="col-md-12">	
+					<h4><span>Headset</span></h4>
+				</div>
+				<div class="sub-cateogy col-md-12">
+					
+
+					<div class="col-sm-3 col-md-3">
+						<div class="accessories-body">
+							<div class="accessories-panel wow fadeInLeft" data-wow-delay="0.2s" >
+								<img src="images/headset/headset1.png" class="img-responsive" alt="" />
+							</div>
+							<div class="accessories-footer">
+									<h3>Sterio Headphone</h3>
+								</div>
+							</div>
+					</div>
+					<div class="col-sm-3 col-md-3">
+						<div class="accessories-body">
+							<div class="accessories-panel wow fadeInLeft" data-wow-delay="0.2s" >
+								<img src="images/headset/headset3.png" class="img-responsive" alt="" />
+							</div>
+							<div class="accessories-footer">
+									<h3>Sterio Headphone</h3>
+								</div>
+							</div>
+					</div>
+					<div class="col-sm-3 col-md-3">
+						<div class="accessories-body">
+							<div class="accessories-panel wow fadeInLeft" data-wow-delay="0.2s" >
+								<img src="images/headset/headset5.png" class="img-responsive" alt="" />
+							</div>
+							<div class="accessories-footer">
+									<h3>Sterio Headphone</h3>
+								</div>
+							</div>
+					</div>
+					<div class="col-sm-3 col-md-3">
+						<div class="accessories-body">
+							<div class="accessories-panel wow fadeInLeft" data-wow-delay="0.2s" >
+								<img src="images/headset/headset8.png" class="img-responsive" alt="" />
+							</div>
+							<div class="accessories-footer">
+									<h3>Sterio Headphone</h3>
+								</div>
+							</div>
+					</div>
+					<div class="col-sm-3 col-md-3">
+						<div class="accessories-body">
+							<div class="accessories-panel wow fadeInLeft" data-wow-delay="0.2s" >
+								<img src="images/headset/headset9.png" class="img-responsive" alt="" />
+							</div>
+							<div class="accessories-footer">
+									<h3>Sterio Headphone</h3>
+								</div>
+							</div>
+					</div>
+					<div class="col-sm-3 col-md-3">
+						<div class="accessories-body">
+							<div class="accessories-panel wow fadeInLeft" data-wow-delay="0.2s" >
+								<img src="images/headset/headset11.png" class="img-responsive" alt="" />
+							</div>
+							<div class="accessories-footer">
+									<h3>Sterio Headphone</h3>
+								</div>
+							</div>
+					</div>
+
+					<div class="col-sm-3 col-md-3">
+						<div class="accessories-body">
+							<div class="accessories-panel wow fadeInLeft" data-wow-delay="0.2s" >
+								<img src="images/headset/headset13.jpg" class="img-responsive" alt="" />
+							</div>
+							<div class="accessories-footer">
+									<h3>Sterio Headphone Hifi</h3>
+							</div>
+						</div>
+					</div>
+					<div class="col-sm-3 col-md-3">
+						<div class="accessories-body">
+							<div class="accessories-panel wow fadeInLeft" data-wow-delay="0.2s" >
+								<img src="images/headset/headset15.png" class="img-responsive" alt="" />
+							</div>
+							<div class="accessories-footer">
+									<h3>Sterio Hifi Headphone HP-47</h3>
+							</div>
+						</div>
+					</div>
+					<div class="col-sm-3 col-md-3">
+						<div class="accessories-body">
+							<div class="accessories-panel wow fadeInLeft" data-wow-delay="0.2s" >
+								<img src="images/headset/headset17.png" class="img-responsive" alt="" />
+							</div>
+							<div class="accessories-footer">
+									<h3>Sterio Hifi Headphone HP-47</h3>
+							</div>
+						</div>
+					</div>
+				
+					<div class="col-sm-3 col-md-3">
+						<div class="accessories-body">
+							<div class="accessories-panel wow fadeInLeft" data-wow-delay="0.2s" >
+								<img src="images/headset/headset19.png" class="img-responsive" alt="" />
+							</div>
+							<div class="accessories-footer">
+									<h3>Sterio in Ear Headphone CV-30</h3>
+							</div>
+						</div>
+					</div>
+					<div class="col-sm-3 col-md-3">
+						<div class="accessories-body">
+							<div class="accessories-panel wow fadeInLeft" data-wow-delay="0.2s" >
+								<img src="images/headset/headset22.png" class="img-responsive" alt="" />
+							</div>
+							<div class="accessories-footer">
+									<h3>Sterio In Ear Headphone CV-27</h3>
+							</div>
+						</div>
+					</div>
+
+				</div>
+
+				</div>				
+			</div>
+		</div>
+							</div>
+							
+						</div>
+								
+				</div>				
+			</div>
+		</div>
+	<!--eNd fEatured pRoduct Area -->
+
+	
+	<?php include("footer.php"); ?>
