f66bff22ab682edb6f345a37e84efb0813c33e45
rohitsraj12 <rohitsraj12@gmail.com>
2020-12-27 09:45:57 +0530

    Create charger.php

---
+++ public/charger.php
@@ -0,0 +1,105 @@
+<?php include("header.php") ?>
+	
+ 
+
+
+	<!-- fEatured pRoduct Area -->
+		<div class="inner-page">
+			<div class="container-fluid">
+				<div class="row">	
+					<div class="inner-banner">
+						<img src="images/inner-banner.jpg" class="img-responsive"/>
+					</div>
+					<div class="inner-page-content">
+					
+							<div class="col-md-12">
+								<!-- cRovell pRoduct aRea-->
+		<div class="accessories-section">
+			<div class="container">
+				<div class="row">
+				<div class="col-md-12">	
+					<h4><span>Chargers</span></h4>
+				</div>
+				<div class="col-md-12">
+					<div class="col-sm-3 col-md-3">
+						<div class="accessories-body">
+							<div class="accessories-panel wow fadeInLeft" data-wow-delay="0.2s" >
+								<img src="images/charger/charger1.jpg" class="img-responsive" alt="" />
+							</div>
+							<div class="accessories-footer">
+								<h3>Mobile Charger</h3>
+							</div>
+						</div>
+					</div>
+				
+					
+				
+				
+					<div class="col-sm-3 col-md-3">
+						<div class="accessories-body">
+							<div class="accessories-panel wow fadeInLeft" data-wow-delay="0.2s" >
+								<img src="images/charger/charger5.png" class="img-responsive" alt="" />
+							</div>
+							<div class="accessories-footer">
+								<h3>mobile adapter</h3>
+							</div>
+						</div>
+					</div>
+				
+
+				
+					<div class="col-sm-3 col-md-3">
+						<div class="accessories-body">
+							<div class="accessories-panel wow fadeInLeft" data-wow-delay="0.4s">
+								<img src="images/charger/charger8.png" class="img-responsive" alt="" />
+							</div>
+							<div class="accessories-footer">
+								<h3>mobile adapter</h3>
+							</div>
+						</div>
+					</div>
+
+					
+					<div class="col-sm-3 col-md-3">
+						<div class="accessories-body">
+							<div class="accessories-panel wow fadeInLeft" data-wow-delay="0.4s">
+								<img src="images/charger/charger10.png" class="img-responsive" alt="" />
+							</div>
+							<div class="accessories-footer">
+								<h3>mobile adapter</h3>
+							</div>
+						</div>
+					</div>
+
+				
+
+					<div class="col-sm-3 col-md-3">
+						<div class="accessories-body">
+							<div class="accessories-panel wow fadeInLeft" data-wow-delay="0.2s" >
+								<img src="images/charger/charger13.png" class="img-responsive" alt="" />
+							</div>
+							<div class="accessories-footer">
+								<h3>mobile adapter</h3>
+							</div>
+						</div>
+					</div>
+				
+					</div>
+				</div>
+
+			
+
+				</div>				
+			</div>
+		</div>
+							</div>
+							
+						</div>
+								
+				</div>				
+			</div>
+		</div>
+	<!--eNd fEatured pRoduct Area -->
+
+	
+	<?php include("footer.php"); ?>
