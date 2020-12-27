10c7757b353768bed3ef03020d7a8ee965c9b28a
rohitsraj12 <rohitsraj12@gmail.com>
2020-12-27 09:54:06 +0530

    Create footer.php

---
+++ public/required/footer.php
@@ -0,0 +1,205 @@
+<?php
+    
+    $base_url = "http://crovell.in/";
+?>
+<!-- gOogle mAp Area -->
+		<div class="google-map-section">
+			<div class="container-fluid">
+				<div class="row">
+					<div class="map-section">
+					<iframe src="https://www.google.com/maps/embed?pb=!1m16!1m12!1m3!1d3773.176351012586!2d72.82158941437523!3d18.96781756029431!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!2m1!1sOrchid+City+Center%2C++Bellasis+Road%2C+Mumbai+Central(E)%2C++Mumbai+%E2%80%93+400+008!5e0!3m2!1sen!2sin!4v1545977364516" width="100%" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>				
+					</div>					
+				</div>				
+			</div>
+		</div>
+	<!-- eNd gOogle mAp Area -->
+	<!-- footer Area -->
+		<div class="footer-fixed">
+			 <div class="scrollup">
+	        <a href="#"><i><img src="<?=$base_url;?>images/up-arrow.png" alt="" /></i></a>
+	    </div><!-- End off scroll up -->
+  <!-- sTart jOin-now sEction -->
+    <div class="join-now">
+      <div class="container">
+        <!-- sTart rOw -->
+        <div class="row">
+          <div class="col-sm-7 col-md-7">
+            <div class="join-now-content">
+              <div class="row">
+               <!--  <div class="col-sm-2 col-md-2">
+                  <img src="images/join-now-icon.png" class="responsive-img" alt=""  />
+                </div> -->
+                <div class="col-sm-10 col-md-10">
+                  <h4 class="">We are 24 hours ready for your support</h4>
+                </div>
+              </div>
+            </div> 
+          </div>
+
+          <div class="col-sm-5 col-md-5">
+            <div class="join-now-form">
+                    <div class="col-md-6 pull-right">
+                        <h1 class="join-btn"><a href="<?=$base_url;?>contact.php">CONNECT WITH US</a></h1>
+                    </div>
+            </div> 
+          </div>
+        </div>
+        <!-- eNd rOw -->           
+      </div>
+    </div>
+  <!-- eNd jOin-no sEction -->
+
+
+  <!-- sTart fOoter sEction -->
+  <div class="footer-section">
+	   <!-- scroll up-->
+   
+  	<div class=""></div>
+      <div class="container">
+        <!-- sTart rOw -->
+        <div class="row">
+          <div class="col-sm-4 col-md-4">
+            <div class="footer-about-secton">
+              <div class="footer-logo">
+                <div class="ftr-logo-img">
+                    <img src="<?=$base_url;?>images/footerlogo.png" class="responsive-img" />
+                 </div>
+                <p>Mobile phones have become no less than a body part to us and it is important that they look like the extension of our personality.</p>
+              </div>
+             
+              <div class="clearfix"></div>
+              
+               <p class="copy-right">2018 All Rights Reserved © Crovell</p>
+            </div> 
+          </div>  
+
+          <div class="col-sm-8 col-md-8">
+          	<div class="footer-search-by">	 
+          		<div class="col-sm-7 col-md-7">	                      
+	              	<div class="row">
+		                <div class="col-sm-6 col-md-6">
+		                	  <h4>Information:</h4>
+		                  <ul> 
+		                    <li><a href="<?=$base_url;?>about.php" title="">About Us </a></li>
+		                    <li><a href="<?=$base_url;?>contact.php" title="">Contact Us </a></li>
+		                    <li><a href="<?=$base_url;?>mobile-accessories.php" title="">Mobile Accessories </a></li>
+		                    <li><a href="#" title="">Privacy Policy </a></li>
+		                   
+		                  </ul>
+		                </div>
+
+		                <div class="col-sm-6 col-md-6">
+		                	<h4>Follow Us:</h4>
+			                <ul> 
+			                    <li><a href="https://www.facebook.com/crovell.impex.16" title="" target="blank">Facebook </a></li>
+			                    <li><a href="https://twitter.com/Crovell1" title="" target="blank">Twitter</a></li>
+			                    <li><a href="https://www.instagram.com/crovellpage/" title="" target="blank">Instagram </a></li>	
+			                     <li><a href="https://www.youtube.com/channel/UCVSlan3y89NlrRKVZRMCB_w" title="" target="blank">Youtube </a></li>                    
+			                </ul>
+		                </div>
+		            </div>
+	            </div> 
+	           
+	            <div class="col-sm-5 col-md-5">
+		            <div class="">
+		               
+			             	<h4>Address</h4>	
+			             	<h5>Crovell Impex</h5>
+			                <p>Orchid City Center,<br> Shop No. C2-573 & C2-574,  <br>2nd Floor, Opp. BEST Bus Depot,<br>
+								Bellasis Road, Mumbai Central(E), <br>Mumbai – 400 008.
+								</p>
+			        </div> 
+		        </div>
+		        <div class="col-sm-12 col-md-12">
+		            <div class="">
+		                <div class="footer-contact">			             	
+			               <h4>Contact Details</h4>	- <p><span>9867509840</span> | <span>9869935710</span> | <span>022-6188 6573</span></p>
+			            </div>
+		            </div> 
+		        </div>
+
+	        </div>
+          </div> 
+        </div>
+        <!-- eNd rOw -->           
+      </div>
+    </div>
+  </div>
+  <!-- eNd fOoter sEction -->
+  <div class="clearfix"></div>
+	<!-- eNd footer Area -->
+	</body>
+
+	  <!-- JavaScript -->
+        <script src="http://code.jquery.com/jquery-1.12.1.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+        <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/wow/1.1.2/wow.js"></script>
+ 		<!-- <script src="js/easing.min.js"></script> -->
+        <script src="<?=$base_url;?>js/jquery.nivo.slider.js" type="text/javascript"></script>
+        <script src="https://ajax.aspnetcdn.com/ajax/jquery.validate/1.14.0/jquery.validate.min.js"></script> 
+         <script type="text/javascript" src="<?=$base_url;?>js/custom-validation.js"></script>
+        <!--main js-->
+        <script type="text/javascript" src="<?=$base_url;?>js/custom.js"></script>
+
+        <script>
+        	 $(function(){
+		      new WOW().init(); 
+		    });
+
+			$(window).scroll(function(){
+				if($(window).scrollTop() > 100){
+					$(".social-icon").fadeIn("slow");			
+			}
+		});
+
+		$(window).scroll(function(){
+			if($(window).scrollTop() < 100){
+			$(".social-icon").fadeOut("slow");	
+		}
+		});
+		</script>
+        <script>
+		      window.onscroll = function() {myFunction()};
+
+		      var header = document.getElementById("myHeader");
+		      var sticky = header.offsetTop;
+
+		      function myFunction() {
+		        if (window.pageYOffset > sticky) {
+		          header.classList.add("sticky");
+		        } else {
+		          header.classList.remove("sticky");
+		        }
+		      }
+		    </script>
+		    <script type="text/javascript">
+		    	// scroll Up
+
+				    // $(window).scroll(function () {
+				    //     if ($(this).scrollTop() > 600) {
+				    //         $('.scrollup').fadeIn('slow');
+				    //     } else {
+				    //         $('.scrollup').fadeOut('slow');
+				    //     }
+				    // });
+				    $('.scrollup').click(function () {
+				        $("html, body").animate({scrollTop: 0}, 1000);
+				        return false;
+				    });
+
+
+		    </script>
+
+		    <script type="text/javascript">
+		    	$(document).ready(function(){
+					  $("sub-menu").click(function(){
+					   $('.dropdwn').addClass('active');
+					  });
+					});
+		    	 //   $('.sub-menu').click(function (){
+				    //     console.log('hi');
+				    //     $('.dropdwn').addClass('active');
+				    // });
+
+		    </script>
+</html>
