f3eb4c287f45b7e17fe338fb67ece37102662c89
rohitsraj12 <rohitsraj12@gmail.com>
2020-12-27 09:51:51 +0530

    Create index.php

---
+++ public/index.php
@@ -0,0 +1,496 @@
+<?php 
+	// $base_url = "http://crovell.in";
+// $base_url = "http://localhost/project/CROVELL-WEBSITE/public/";
+	
+	// include("../private/required/public/public_header.php");
+	include("../private/required/public/public_header.php");
+
+ ?>
+	 <!-- Start Slider Area -->
+	 <div class="">
+	<div id="home" class="slider-area">
+		<div class="bend niceties preview-2">
+			<div id="ensign-nivoslider" class="slides">
+				<img src="<?=base_url();?>img/banner/banner-1.jpg" class="slider-1" alt="" title="#slider-direction-1" />
+				<img src="<?=base_url();?>img/banner/banner-2.jpg" alt="" title="#slider-direction-2" />
+				<img src="<?=base_url();?>img/banner/banner-3.jpg" alt="" title="#slider-direction-3" />
+			</div>
+		</div>
+	</div>
+	</div>
+  <!-- End Slider Area -->
+	<!-- bAnner Area-->
+		<!-- <div class="banner">
+			<div class="container-fluid">
+				<div class="row">		
+				<img src="images/banner.jpg" class="img-responsive" />			
+				</div>				
+			</div>
+		</div> -->
+		
+	<!--eNd bAnner Area-->
+
+	<!-- aBout uS Area-->		
+			<section id="about">
+	            <div class="container">
+	                <div class="row">
+
+	                    <div class="col-md-9">
+	                        <div class="about_content">
+	                            <img src="images/about_img.jpg" alt="" class="about_img" />
+	                            <h2>Welcome To CROVELL</h2>
+
+	                            <p>
+								The Mumbai-based Crovell, a new brand of mobile accessories is all set to be launched in November 2019 with a range of innovative and latest products. 
+									Mobile phones have become no less than a body part to us and it is important that they look like the extension of our personality. Dress them up in your favorite colors, patterns, and styles or let them display your favorite quotations and sayings. At Crovell.in Pick out the best mobile phone covers. Look out for the best and latest mobile accessories online in India. 
+								</p>
+
+	                            <a href="about.php" class="btn know_btn">Know More</a>
+	                        </div>
+	                    </div>
+	                    <div class="col-md-3">
+	                        <div class="yoga_content">
+	                            <div class="yoga">
+	                                <h3>Love Music <span>&xlarr;</span></h3>
+	                                <p>Music is a love of life Headphone,Earphone etc.</p>
+
+	                               <!--  <a href="" class="btn link_btn">Know More</a> -->
+	                            </div>
+	                            <div class="yoga_banner">
+	                                <img src="images/yoga_img.jpg" alt="Yoga" class="yoga_img" />
+	                                <div class="photo_overlay"></div>
+	                            </div>					
+	                        </div>
+	                    </div>
+
+	                </div>
+	            </div>
+	        </section>
+	<!--eNd aBout uS Area-->
+
+	
+
+		<!-- cRovell pRoduct aRea-->
+		<div class="accessories-section">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-12">	
+						<h4><span>BLUETOOTH</span> Accessories</h4>
+					</div>
+					<div class="col-md-12">
+						<div class="row">
+							<div class="col-md-6">
+								<div class="row">
+									<div class="col-sm-6">
+										<a href="<?=base_url();?>products/bluetooth_earphone/index.php">
+											<div class="accessories-body">
+												<div class="accessories-panel wow bounceInDown" data-wow-delay="0.8s" >
+													<img src="<?=base_url();?>img/products/bluetooth_earphone/CV_08_bluetooth_earphone.jpeg" class="img-responsive" alt="" />
+												</div>
+												<div class="accessories-footer">
+													<h3>bluetooth earphone </h3>
+												</div>
+											</div>
+										</a>
+									</div>
+									<div class="col-sm-6">
+											
+										<a href="<?=base_url();?>products/bluetooth_headphone/index.php">
+											<div class="accessories-body">
+												<div class="accessories-panel wow bounceInUp" data-wow-delay="0.8s">
+													<img src="<?=base_url();?>img/products/bluetooth_headphone/cv_h52_headphone.jpeg" class="img-responsive" alt="" />
+												</div>
+												<div class="accessories-footer">
+													<h3>bluetooth headphone</h3>
+												</div>
+											</div>
+										</a>
+									</div>
+									<div class="col-sm-6">
+										<a href="<?=base_url();?>products/bluetooth_airpord/index.php">
+											<div class="accessories-body">
+												<div class="accessories-panel wow bounceInDown" data-wow-delay="0.8s">
+													<img src="<?=base_url();?>img/products/bluetooth_airpord/cv_al_11_airpord.jpeg" class="img-responsive" alt="" />
+												</div>
+												<div class="accessories-footer">
+													<h3>Bluetooth airpord</h3>
+												</div>
+											</div>
+										</a>
+									</div>	
+									<div class="col-sm-6">
+										<a href="<?=base_url();?>products/bluetooth_dongal/index.php">
+											<div class="accessories-body">
+												<div class="accessories-panel wow bounceInUp" data-wow-delay="0.8s">
+													<img src="<?=base_url();?>img/products/bluetooth_dongal/CV_04_bluetooth_dongal.jpeg" class="img-responsive" alt="" />
+												</div>
+												<div class="accessories-footer">
+													<h3>Bluetooth dongal</h3>
+												</div>
+											</div>
+										</a>
+									</div>
+								</div>
+							</div>
+							
+							<div class="col-sm-6">
+								<a href="<?=base_url();?>products/bluetooth_speaker/index.php">
+									<div class="accessories-body">
+										<div class="accessories-panel wow bounceInDown" data-wow-delay="0.8s" >
+											<img src="<?=base_url();?>img/products/bluetooth_speaker/cv_115_bt_speker.jpeg" class="img-responsive" alt="" />
+										</div>
+										<div class="accessories-footer">
+											<h3>BLUETOOTH speaker</h3>
+										</div>
+									</div>
+								</a>
+							</div>
+						</div>
+					</div>
+				
+				</div>
+
+				</div>				
+			</div>
+		</div>
+
+		
+		<div class="accessories-section">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-12">	
+						<h4><span>CAR</span> Accessories</h4>
+					</div>
+					<div class="col-md-12">
+						<div class="row">
+							<div class="col-md-6">
+								<div class="row">
+									<div class="col-sm-6">
+										<a href="<?=base_url();?>products/bluetooth_earphone/index.php">
+											<div class="accessories-body">
+												<div class="accessories-panel wow bounceInDown" data-wow-delay="0.8s" >
+													<img src="<?=base_url();?>img/products/bluetooth_earphone/CV_08_bluetooth_earphone.jpeg" class="img-responsive" alt="" />
+												</div>
+												<div class="accessories-footer">
+													<h3>bluetooth earphone </h3>
+												</div>
+											</div>
+										</a>
+									</div>
+									<div class="col-sm-6">
+											
+										<a href="<?=base_url();?>products/bluetooth_headphone/index.php">
+											<div class="accessories-body">
+												<div class="accessories-panel wow bounceInUp" data-wow-delay="0.8s">
+													<img src="<?=base_url();?>img/products/bluetooth_headphone/cv_h52_headphone.jpeg" class="img-responsive" alt="" />
+												</div>
+												<div class="accessories-footer">
+													<h3>bluetooth headphone</h3>
+												</div>
+											</div>
+										</a>
+									</div>
+									<div class="col-sm-6">
+										<a href="<?=base_url();?>products/bluetooth_airpord/index.php">
+											<div class="accessories-body">
+												<div class="accessories-panel wow bounceInDown" data-wow-delay="0.8s">
+													<img src="<?=base_url();?>img/products/bluetooth_airpord/cv_al_11_airpord.jpeg" class="img-responsive" alt="" />
+												</div>
+												<div class="accessories-footer">
+													<h3>Bluetooth airpord</h3>
+												</div>
+											</div>
+										</a>
+									</div>	
+									<div class="col-sm-6">
+										<a href="<?=base_url();?>products/bluetooth_dongal/index.php">
+											<div class="accessories-body">
+												<div class="accessories-panel wow bounceInUp" data-wow-delay="0.8s">
+													<img src="<?=base_url();?>img/products/bluetooth_dongal/CV_04_bluetooth_dongal.jpeg" class="img-responsive" alt="" />
+												</div>
+												<div class="accessories-footer">
+													<h3>Bluetooth dongal</h3>
+												</div>
+											</div>
+										</a>
+									</div>
+								</div>
+							</div>
+							
+							<div class="col-sm-6">
+								<a href="<?=base_url();?>products/bluetooth_speaker/index.php">
+									<div class="accessories-body">
+										<div class="accessories-panel wow bounceInDown" data-wow-delay="0.8s" >
+											<img src="<?=base_url();?>img/products/bluetooth_speaker/cv_115_bt_speker.jpeg" class="img-responsive" alt="" />
+										</div>
+										<div class="accessories-footer">
+											<h3>BLUETOOTH speaker</h3>
+										</div>
+									</div>
+								</a>
+							</div>
+						</div>
+					</div>
+				
+				</div>
+
+				</div>				
+			</div>
+		</div>
+
+		   <!-- Coaches -->
+		   <!-- hide -->
+        <!-- <div id="coaches" class="coaches">
+            <div class="container">
+            	<div class="col-md-12">	
+					<h4><span>Our</span> Products</h4>
+				</div>
+                <div class="coaches_content">
+                    <div class="size_180 wow tada coach_item" data-wow-delay="0.8s">
+                    	<img src="images/coach_img10.png" alt="" />
+                       
+                    </div>
+                    <div class="size_180 coach_item wow bounceIn" data-wow-delay="0.2s">
+                        <img src="images/coach_img1.png" alt="" />
+                    </div>
+                    <div class="size_180 coach_item wow bounceIn" data-wow-delay="1.4s">
+                        <img src="images/coach_img9.png" alt="" />
+                    </div>
+                    <div class="size_180 coach_item wow bounceIn" data-wow-delay="0.8s">
+                        <img src="images/coach_img3.png" alt="" />
+                    </div>
+                    <div class="size_180 wow tada coach_item" data-wow-delay="0.8s">
+                    	<img src="images/coach_img11.png" alt="" />
+                    </div>
+                    <div class="size_180 coach_item wow bounceIn" data-wow-delay="0.6s">
+                        <img src="images/coach_img4.png" alt="" />
+                    </div>
+                    <div class="size_180 coach_item wow bounceIn" data-wow-delay="1s">
+                        <img src="images/coach_img5.png" alt="" />
+                    </div>
+                    <div class="size_180 coach_item wow bounceIn" data-wow-delay="1.2s">
+                        <img src="images/coach_img6.png" alt="" />
+                    </div>
+                     <div class="size_180 wow tada coach_item" data-wow-delay="0.8s">
+                    	<img src="images/coach_img13.png" alt="" />
+                    </div>
+                    <div class="size_180 coach_item wow bounceIn" data-wow-delay="1.4s">
+                       	<img src="images/coach_img12.png" alt="" />
+                    </div>
+                    <div class="size_180 coach_item wow bounceIn" data-wow-delay="1.6s">
+                        <img src="images/coach_img8.png" alt="" />
+                    </div>
+                    <div class="size_180 coach_item wow bounceIn" data-wow-delay="1.8s">
+                        <img src="images/coach_img1.png" alt="" />
+                    </div>
+                    <div class="size_180 coach_item">
+                        <img src="images/coach_img7.png" alt="" />
+                    </div>			
+                </div>
+            </div>
+        </div> -->
+		<!-- Coaches end -->
+	<!--eNd cRovell pRoduct Area-->		
+
+	<!-- fEatured pRoduct Area -->
+		<div class="section-testimonial">
+			<div class="container">
+				<div class="">
+					<header class="text-center">
+						<h4 class="h2">Testimonial</h4>
+					</header>
+					<div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+						<!-- Indicators -->
+						<ol class="carousel-indicators">
+							<li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
+							<li data-target="#carousel-example-generic" data-slide-to="1"></li>
+							<li data-target="#carousel-example-generic" data-slide-to="2"></li>
+						</ol>
+
+						<!-- Wrapper for slides -->
+						<div class="carousel-inner" role="listbox">
+							<div class="item active">
+								<div class="py-5 testimonial-body">
+									<div class="row">
+										<div class="col-sm-2">
+											<figure>
+												<img src="<?=base_url();?>images/real_estate_service.png" alt="">
+											</figure>
+										</div>
+										<div class="col-sm-10 testimonial-content-body">
+											<div class="top">
+												<p>
+													Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut, eos vitae. Porro expedita facilis magnam corporis aperiam sunt assumenda rem quam quaerat omnis error officia, ipsa totam et quas laudantium!		
+												</p>
+											</div>
+											<div class="testimonial-footer">
+												<ul>
+												<li>
+													Abhishek Bharatan
+												</li>
+												<li>
+													Andheri, Mumbai
+												</li>
+												</ul>
+											</div>
+										</div>
+									</div>
+								</div>
+							</div>
+							<div class="item">
+								<div class="py-5 testimonial-body">
+									<div class="row">
+										<div class="col-sm-2">
+											<figure>
+												<img src="<?=base_url();?>images/real_estate_service.png" alt="">
+											</figure>
+										</div>
+										<div class="col-sm-10 testimonial-content-body">
+											<div class="top">
+												<p>
+													Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut, eos vitae. Porro expedita facilis magnam corporis aperiam sunt assumenda rem quam quaerat omnis error officia, ipsa totam et quas laudantium!		
+												</p>
+											</div>
+											<div class="testimonial-footer">
+												<ul>
+												<li>
+													Abhishek Bharatan
+												</li>
+												<li>
+													Andheri, Mumbai
+												</li>
+												</ul>
+											</div>
+										</div>
+									</div>
+								</div>
+							</div>
+							<div class="item">
+								<div class="py-5 testimonial-body">
+									<div class="row">
+										<div class="col-sm-2">
+											<figure>
+												<img src="<?=base_url();?>images/real_estate_service.png" alt="">
+											</figure>
+										</div>
+										<div class="col-sm-10 testimonial-content-body">
+											<div class="top">
+												<p>
+													Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut, eos vitae. Porro expedita facilis magnam corporis aperiam sunt assumenda rem quam quaerat omnis error officia, ipsa totam et quas laudantium!		
+												</p>
+											</div>
+											<div class="testimonial-footer">
+												<ul>
+												<li>
+													Abhishek Bharatan
+												</li>
+												<li>
+													Andheri, Mumbai
+												</li>
+												</ul>
+											</div>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+
+						<!-- Controls -->
+						<!-- <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
+							<span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+							<span class="sr-only">Previous</span>
+						</a>
+						<a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
+							<span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+							<span class="sr-only">Next</span>
+						</a> -->
+						</div>
+								
+								<!-- <div class="col-md-12 py-5 testimonial-body">
+									<div class="row">
+										<div class="col-sm-3">
+											<figure>
+												<img src="<?=base_url();?>images/real_estate_service.png" alt="">
+											</figure>
+										</div>
+										<div class="col-sm-9 testimonial-content-body">
+											<div class="top">
+												<p>
+													Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut, eos vitae. Porro expedita facilis magnam corporis aperiam sunt assumenda rem quam quaerat omnis error officia, ipsa totam et quas laudantium!		
+												</p>
+											</div>
+											<div class="testimonial-footer">
+												<ul>
+												<li>
+													Abhishek Bharatan
+												</li>
+												<li>
+													Andheri, Mumbai
+												</li>
+												</ul>
+											</div>
+										</div>
+									</div>
+								</div> -->
+							</div>
+						</div>
+					</div>
+				</div>				
+			</div>
+		</div>
+	<!--eNd fEatured pRoduct Area -->
+
+	<!-- gAllery Area -->
+			<!-- <div class="gallery-section">
+				<div class="container">
+					<div class="">
+					<div class="col-md-12">	
+						<h4><span>Our</span> Gallery</h4>
+					</div>
+					<div class="col-sm-3 col-md-3">
+						<div class="gallery-panel">
+							<div class="gallery-img">
+								<img src="images/pic01.jpg" class="img-responsive wow bounceInLeft" data-wow-delay="0.4s" alt="Coach" />
+							</div>
+							<div class="gallery-img">
+								<img src="images/pic02.jpg" class="img-responsive wow bounceInUp" data-wow-delay="2.2s" alt="Coach" />
+							</div>
+						</div>
+					</div>	
+					<div class="col-sm-3 col-md-3">
+						<div class="gallery-panel">
+							<div class="gallery-img">
+								<img src="images/pic04.jpg" class="img-responsive wow bounceIn" data-wow-delay="2.6s" alt="Coach" />
+							</div>
+							<div class="gallery-img">
+								<img src="images/pic03.jpg" class="img-responsive wow bounceIn" data-wow-delay="0.8s" alt="Coach" />
+							</div>
+						</div>
+					</div>
+					<div class="col-sm-3 col-md-3">
+						<div class="gallery-panel">
+							<div class="gallery-img">
+								<img src="images/pic05.jpg" class="img-responsive wow bounceIn" data-wow-delay="1.2s" alt="Coach" />
+							</div>
+							<div class="gallery-img">
+								<img src="images/pic06.jpg" class="img-responsive wow bounceIn" data-wow-delay="3s" alt="Coach" />
+							</div>
+						</div>
+					</div>
+					<div class="col-sm-3 col-md-3">
+						<div class="gallery-panel">
+							<div class="gallery-img">
+								<img src="images/pic08.jpg" class="img-responsive wow bounceInDown" data-wow-delay="3.4s" alt="Coach" />
+							</div>
+							<div class="gallery-img">
+								<img src="images/pic07.jpg" class="img-responsive wow bounceInRight" data-wow-delay="1.6s" alt="Coach" />
+							</div>
+						</div>
+					</div>
+
+					</div>				
+				</div>
+			</div> -->
+	<!--eNd gAllery Area -->
+
+	<?php include("../private/required/public/public_footer.php"); ?>
