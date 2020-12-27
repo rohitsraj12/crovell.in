89ea0e74390a9b5ea630b7d1cead3de1365e0b52
rohitsraj12 <rohitsraj12@gmail.com>
2020-12-27 09:45:29 +0530

    Create product_list.php

---
+++ private/required/public/product/product_list.php
@@ -0,0 +1,10 @@
+<div class="col-sm-3 col-md-3">
+    <div class="accessories-body">
+        <div class="accessories-panel wow fadeInTop" data-wow-delay="0.2s" >
+            <img style="height: 300px; width: 100%"  src="<?=base_url() . $row['product_image'];?>" class="img-responsive" alt="<?php echo $row['product_name'];?>" />
+        </div>
+        <div class="accessories-footer">
+            <h3><?php echo $row['product_model'];?> <?php echo $row['product_name'];?></h3>
+        </div>
+    </div>
+</div>
