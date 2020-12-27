1d73f80e59010d21bbb17140acd1942c7ca54b67
rohitsraj12 <rohitsraj12@gmail.com>
2020-12-27 09:45:48 +0530

    Create car-charger.php

---
+++ public/car-charger.php
@@ -0,0 +1,65 @@
+<?php include("header.php") ?>
+	
+ 
+
+
+	<!-- fEatured pRoduct Area -->
+		<div class="inner-page">
+			<div class="container-fluid">
+				<div class="row">	
+					<div class="inner-banner">
+						<img src="images/inner-banner.jpg" class="img-responsive"/>
+					</div>
+					<div class="inner-page-content">
+					
+							<div class="col-md-12">
+								<!-- cRovell pRoduct aRea-->
+		<div class="accessories-section">
+			<div class="container">
+				<div class="row">
+				<div class="col-md-12">	
+					<h4><span>Car Chargers</span></h4>
+				</div>
+				<div class="sub-cateogy col-md-12">
+					<div class="col-md-12">	
+						<h6><span></span></h6>
+					</div>
+
+					<div class="col-sm-3 col-md-3">
+						<div class="accessories-body">
+							<div class="accessories-panel wow fadeInLeft" data-wow-delay="0.2s" >
+								<img src="images/car-charger/car-charger1.png" class="img-responsive" alt="" />
+							</div>
+							<div class="accessories-footer">
+								<h3>Car Charger 4IN1 CL-34</h3>
+							</div>
+						</div>
+					</div>
+					<div class="col-sm-3 col-md-3">
+						<div class="accessories-body">
+							<div class="accessories-panel wow fadeInLeft" data-wow-delay="0.2s" >
+								<img src="images/car-charger/car-charger6.png" class="img-responsive" alt="" />
+							</div>
+							<div class="accessories-footer">
+								<h3>Car Charger CL-109</h3>
+							</div>
+						</div>
+					</div>
+				
+				
+				</div>
+				
+				</div>				
+			</div>
+		</div>
+							</div>
+							
+						</div>
+								
+				</div>				
+			</div>
+		</div>
+	<!--eNd fEatured pRoduct Area -->
+
+	
+	<?php include("footer.php"); ?>
