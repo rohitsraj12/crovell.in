fe40982f1669defab652bc690aa4884cd54ad52c
rohitsraj12 <rohitsraj12@gmail.com>
2020-12-27 09:59:14 +0530

    Create index.php

---
+++ public/products/usb_cables/index.php
@@ -0,0 +1,69 @@
+<?php 
+$web_url = "http://crovell.in";
+include("../../../private/db_connection/db_connect.php");
+include($web_url . "/header.php"); 
+
+?>
+	
+ 
+
+
+	<!-- fEatured pRoduct Area -->
+		<div class="inner-page">
+			<div class="container-fluid">
+				<div class="row">	
+					<div class="inner-banner">
+						<img src="images/inner-banner.jpg" class="img-responsive"/>
+					</div>
+					<div class="inner-page-content">
+					
+							<div class="col-md-12">
+								<!-- cRovell pRoduct aRea-->
+		<div class="accessories-section">
+			<div class="container">
+				<div class="row">
+				<div class="col-md-12">	
+					<h4><span>Cable</span></h4>
+				</div>
+				<div class="sub-cateogy col-md-12">
+				
+
+					<div class="col-sm-3 col-md-3">
+						<div class="accessories-body">
+							<div class="accessories-panel wow fadeInLeft" data-wow-delay="0.2s" >
+								<img src="images/cable/cable1.png" class="img-responsive" alt="" />
+							</div>
+							<div class="accessories-footer">
+								<h3>Charger Cable</h3>
+							</div>
+						</div>
+					</div>
+
+					<div class="col-sm-3 col-md-3">
+						<div class="accessories-body">
+							<div class="accessories-panel wow fadeInLeft" data-wow-delay="0.2s" >
+								<img src="images/cable/cable5.png" class="img-responsive" alt="" />
+							</div>
+							<div class="accessories-footer">
+								<h3>USB Cable</h3>
+							</div>
+						</div>
+					</div>
+					
+					
+				</div>
+			
+				</div>				
+			</div>
+		</div>
+							</div>
+							
+						</div>
+								
+				</div>				
+			</div>
+		</div>
+	<!--eNd fEatured pRoduct Area -->
+
+	
+	<?php include($web_url . "footer.php"); ?>
