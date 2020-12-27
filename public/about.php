d4931b5d1ad77ec17dd8651b016b7ffa89ab9f2d
rohitsraj12 <rohitsraj12@gmail.com>
2020-12-27 09:45:38 +0530

    Create about.php

---
+++ public/about.php
@@ -0,0 +1,46 @@
+<?php 
+	
+	include("../private/required/public/public_header.php");
+?>
+	
+ 
+
+
+	<!-- fEatured pRoduct Area -->
+		<div class="inner-page">
+			<div class="container-fluid">
+				<div class="row">	
+					<div class="inner-banner">
+						<img src="images/inner-banner.jpg" class="img-responsive"/>
+					</div>
+					<div class="inner-page-content">
+						<div class="container">
+						<div class="row">
+							<div class="col-md-7">
+								<div class="fadeInRight" data-wow-delay="0.8s">	                       
+	                            <h4>About <span>CROVELL</span></h4>
+
+		                        <p>The Mumbai-based Crovell, a new brand of mobile accessories is all set to be launched in November 2019 with a range of innovative and latest products. </p>
+
+								<p>Mobile phones have become no less than a body part to us and it is important that they look like the extension of our personality. Dress them up in your favorite colors, patterns, and styles or let them display your favorite quotations and sayings. At Crovell.in Pick out the best mobile phone covers. Look out for the best and latest mobile accessories online in India. You can also find data cables, Bluetooth, speakers, chargers, and many more accessories. You can also look for power-banks, screen-guards, selfie-sticks, cradles and docks and even arm-bands. You can find the best of imported mobile accessories only on Crovell.com.</p>
+								<p>Mahenra Mali, Director at Crovell, reveals, “Though Crivell is a new brand, the team have been in the mobile accessories industry for the past 2 decades and know ins and outs of this business thoroughly—from product management to sales and channel management. With smartphones usage growing rapidly, the demand for accessories is also growing proportionately. In general, a smartphone needs 3 or 4 accessories like adaptors, cables, car chargers, headphones, etc. There are very few accessory brands that sell across India; most sell in grey market without any specific targeted segment. Though 80% accessories are sold in grey market in India, we see a lot of scope still for new and white brands, as there is still a big gap in the market which needs to be filled. Our brand Crovell offers gadgets with latest technology with great quality. We are also going to provide accessories similar to those being provided by premium brand, but at a much lesser prices.”</p>
+								 
+								</p>
+	                        </div>
+							</div>
+							<div class="col-md-5">
+								<div class="wow fadeInRight" data-wow-delay="0.8s">
+	                                <img src="images/about-inner-banner.jpg" alt="Yoga" class="img-responsive" />
+	                                <div class="photo_overlay"></div>
+	                            </div>	
+							</div>
+						</div>
+					</div>
+					</div>					
+				</div>				
+			</div>
+		</div>
+	<!--eNd fEatured pRoduct Area -->
+
+	
+	<?php include("../private/required/public/public_footer.php"); ?>
